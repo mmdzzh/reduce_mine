d4aa159f1942948a970bbb92d0a2428bd58e21d4
mmdzzh <295885671@qq.com>
2022-06-24 16:10:39 +0800

    111

--- py_reduce.c
+++ py_reduce.c
@@ -4,9 +4,6 @@
 #include <stdio.h>
 #include <immintrin.h>
 #include <nmmintrin.h>
-static PyMethodDef SumMethods[] = {
-    {NULL, NULL, 0, NULL}
-};
 
 float eps_sum_sse(float* tmp, int i, int j){
     float eps = 0.0;
@@ -18,13 +15,22 @@ float eps_sum_sse(float* tmp, int i, int j){
     return eps;
 }
 
-static void sum_zjh_cpu_sse(char **args, npy_intp *dimensions, npy_intp *steps, void *data){
+static PyObject* sum_zjh_cpu_sse(PyObject *self, PyObject *args){
+    PyArrayObject* npObject;
+
+    if(!PyArg_ParseTuple(args, "O", &npObject)){
+        printf("failed\n");
+        return NULL;
+    }
+    
     npy_intp i, j, iend, gap;
-    npy_intp n = dimensions[0];
-    float *input = (float*)args[0];
-    float *tmp = (float*)args[1];
-    // npy_float32* input = (npy_float32*)in;
-    // npy_float32* out = (npy_float32*)ou;
+    npy_intp n = npObject->dimensions[0];
+    
+
+    // dimensions[1] = 1;
+    npy_float32 *input = (npy_float32*)(npObject->data);
+ 
+  
     npy_intp n2 = n / 32;
 
     n2--;
@@ -35,24 +41,32 @@ static void sum_zjh_cpu_sse(char **args, npy_intp *dimensions, npy_intp *steps, 
     n2 |= (n2>>16);
     n2++;
 
-    // float* tmp = (float*)malloc(n2 * sizeof(float));
-    // memset(tmp, 0, sizeof(float) * n2);
+    npy_float32* tmp = (npy_float32*)malloc(n2 * sizeof(npy_float32));
+    memset(tmp, 0, sizeof(npy_float32) * n2);
 
 	__m128 sum1 = _mm_setzero_ps(), sum2 = _mm_setzero_ps(), sum3 = _mm_setzero_ps(), sum4 = _mm_setzero_ps();
 	__m128 load1, load2, load3, load4;
-	for (i = 0; i < n2; i += 16) {
+
+	npy_intp block_num = 8;
+	npy_intp block_size = 16 * block_num;
+    npy_intp k, remain;
+	for (i = 0; i < n2; i += block_size) {
 		j = i;
 		for (; j < n; j += n2) {
-			if(j + 16 < n){
-				load1 = _mm_load_ps(input + j);
-				load2 = _mm_load_ps(input + j + 4);
-				load3 = _mm_load_ps(input + j + 8);
-				load4 = _mm_load_ps(input + j + 12);
+			if(j + block_size < n){
+				#pragma unroll
+				for(k = 0;k < block_size;k += 16){
+					load1 = _mm_load_ps(input + j + k);
+					load2 = _mm_load_ps(input + j + 4 + k);
+					load3 = _mm_load_ps(input + j + 8 + k);
+					load4 = _mm_load_ps(input + j + 12 + k);
 
-				sum1 = _mm_add_ps(sum1, load1);
-				sum2 = _mm_add_ps(sum2, load2);
-				sum3 = _mm_add_ps(sum3, load3);
-				sum4 = _mm_add_ps(sum4, load4);		
+					sum1 = _mm_add_ps(sum1, load1);
+					sum2 = _mm_add_ps(sum2, load2);
+					sum3 = _mm_add_ps(sum3, load3);
+					sum4 = _mm_add_ps(sum4, load4);		
+				}
+
 			}
 			else{
 				break;
@@ -70,8 +84,8 @@ static void sum_zjh_cpu_sse(char **args, npy_intp *dimensions, npy_intp *steps, 
 		sum4 = _mm_setzero_ps();
 
 		if(j < n){
-			int remain = n - j;
-			for(int k = 0;k < remain;k++){
+			remain = n - j;
+			for(k = 0;k < remain;k++){
 				tmp[i + k] += input[j + k];
 			}
 		}
@@ -122,22 +136,22 @@ static void sum_zjh_cpu_sse(char **args, npy_intp *dimensions, npy_intp *steps, 
 			_mm_store_ps(tmp + i + 8, sum3);
 			_mm_store_ps(tmp + i + 12, sum4);
 		}
-        eps += eps_sum(tmp, i, i + gap);
-		float t;
-		if (eps != 0) {
+        // eps += eps_sum(tmp, i, i + gap);
+		// float t;
+		// if (eps != 0) {
             
-			for (i = 0; i < gap; i++) {
-				t = tmp[i] - eps;
-				if (t - tmp[i] + eps == 0) {
-					tmp[i] = t;
-					break;
-				}
-			}
-            if(i == gap){
-                tmp[i-1] -= eps;
-            }
-			eps = 0;
-		}
+		// 	for (i = 0; i < gap; i++) {
+		// 		t = tmp[i] - eps;
+		// 		if (t - tmp[i] + eps == 0) {
+		// 			tmp[i] = t;
+		// 			break;
+		// 		}
+		// 	}
+        //     if(i == gap){
+        //         tmp[i-1] -= eps;
+        //     }
+		// 	eps = 0;
+		// }
 	}
 	sum1 = _mm_add_ps(sum1, sum2);
 	sum3 = _mm_add_ps(sum3, sum4);
@@ -147,9 +161,28 @@ static void sum_zjh_cpu_sse(char **args, npy_intp *dimensions, npy_intp *steps, 
 	// //sum1 = _mm256_hadd_ps(sum1, sum1);
 
 	_mm_store_ps(tmp, sum1);
-    // *out = tmp[0];
-    // free(tmp);
     
+    // PyArrayObject *out = (PyArrayObject*)malloc(sizeof(PyArrayObject));
+    // printf("11111\n");
+    // out->dimensions = (npy_intp*)malloc(sizeof(npy_intp));
+    // printf("22222\n");
+    // out->dimensions[0] = 1;
+    // printf("dimension is %d\n", out->dimensions[0]);
+    // out->data = (npy_float32*)malloc(sizeof(npy_float32));
+    
+    // printf("3333\n");
+    // out->data[0] = tmp[0];
+
+    
+    // printf("out is %f\n", tmp[0]);
+    // printf("out is %f\n", out->data[0]);
+    npy_float32 out = tmp[0];
+    // free(tmp);
+   
+
+    return Py_BuildValue("f", out);
+    // PyObject* ret_test = (PyObject*)malloc(sizeof(PyObject));
+    // return ret_test;//out;
 }
 
 static void sum_zjh_cpu_avx(char **args, npy_intp *dimensions, npy_intp *steps, void *data){
@@ -263,6 +296,11 @@ static npy_float32* sum_zjh_cpu(char **args, npy_intp *dimensions, npy_intp *ste
     return out;
 }
 
+static PyMethodDef SumMethods[] = {
+    {"sum", sum_zjh_cpu_sse, METH_VARARGS, NULL},
+    {NULL, NULL, 0, NULL}
+};
+
 
 PyUFuncGenericFunction funcs[1] = {&sum_zjh_cpu_sse};
 
@@ -285,25 +323,28 @@ static struct PyModuleDef moduledef = {
 };
 PyMODINIT_FUNC PyInit_zjhnp(void)
 {
-    PyObject *m, *sum, *d;
+    PyObject *m;
     m = PyModule_Create(&moduledef);
     if (!m) {
         return NULL;
     }
-
-    import_array();
-    import_umath();
-
-    sum = PyUFunc_FromFuncAndData(funcs, data, types, 1, 1, 1,
-                                    PyUFunc_One, "sum",
-                                    "sum_docstring", 0);
-
-    d = PyModule_GetDict(m);
-
-    PyDict_SetItemString(d, "sum", sum);
-    Py_DECREF(sum);
-
+    // import_array();
+    // import_umath();
+    // Py_DECREF(m);
     return m;
+    // import_array();
+    // import_umath();
+
+    // sum = PyUFunc_FromFuncAndDataAndSignatureAndIdentity(funcs, data, types, 1, 1, 1,
+    //                                 PyUFunc_One, "sum",
+    //                                 "sum_docstring", 0, NULL, PyUFunc_One);
+
+    // d = PyModule_GetDict(m);
+
+    // PyDict_SetItemString(d, "sum", sum);
+    // Py_DECREF(sum);
+
+    // return m;
 }
 #else
 PyMODINIT_FUNC initzjhnp(void)
@@ -313,13 +354,13 @@ PyMODINIT_FUNC initzjhnp(void)
     if (!m) {
         return NULL;
     }
-
+    return m;
     import_array();
     import_umath();
 
-    sum = PyUFunc_FromFuncAndData(funcs, data, types, 1, 1, 1,
-                                    PyUFunc_None, "sum",
-                                    "sum_docstring", 0);
+    sum = PyUFunc_FromFuncAndDataAndSignatureAndIdentity(funcs, data, types, 1, 1, 1,
+                                    PyUFunc_One, "sum",
+                                    "sum_docstring", 0, NULL);
 
     d = PyModule_GetDict(m);
 
